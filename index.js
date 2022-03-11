08d26942f1a17e2be6f597c5ff22a1c45150693c
ElChamo36 <chamorrolmariano@gmail.com>
2022-03-11 14:29:07 -0300

    36

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ const mysql = require('mysql2');
 const conexion = mysql.createConnection({
     host: process.env.HOST,
     user: process.env.USER,
-    password: process.env.PASS,
+    password: process.env.PASSWORD,
     database: process.env.DB,
     port: 3306
 });
